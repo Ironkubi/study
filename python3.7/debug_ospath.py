9d9d3f00d9a086611010cef6ea66c99ca2252c62
Ironkubi <419406847@qq.com>
2021-08-23 13:33:38 +0800

    update

--- python3.7/debug_ospath.py
+++ python3.7/debug_ospath.py
@@ -9,11 +9,20 @@ import os
 # 获取文件所在目录的完整路径
 a = os.path.dirname(__file__)
 print(a)
+print("\n")
 
 # 返回的是.py文件的绝对路径
 print(os.path.abspath(__file__))
 print(os.path.abspath("."))
 print(os.path.abspath(".."))
 print(os.path.abspath("曹挣钱"))
+print("\n")
 
+# 组合使用
+path1 = os.path.dirname(os.path.abspath(__file__))
+print(path1)
+
+# os.path.join拼接路径
+path2 = os.path.join(os.path.dirname(os.path.abspath(__file__)),'1.py')
+print(path2)
 
